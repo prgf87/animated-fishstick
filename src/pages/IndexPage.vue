ee79e8c4afc6192b71d8499749b96f7412c57e4f
Pedro <50713178+prgf87@users.noreply.github.com>
2023-11-30 17:28:09 +0000

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page padding>
+    <input v-model="message" @keyup="handleKeyUp"/>
+    <button @click="clearMessage" >Clear Text</button>
+    <h5>{{ message }}</h5>
+  </q-page>
+</template>
+
+<script lang="ts">
+  export default {
+    data(){
+       return {
+         message: "I am VueJS!"
+       }
+    },
+    methods:{
+      clearMessage(){
+        this.message = ''
+      },
+      handleKeyUp(event){
+        console.log(event)
+      }
+    }
+  }
+</script>
