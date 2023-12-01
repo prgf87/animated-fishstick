3507eb65e8435a82c7432d958820d19c9c4a9793
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-01 22:01:10 +0000

    changing to script setup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,9 +1,18 @@
 <template>
   <q-page padding>
+    <button
+      class="btn"
+      style="position: absolute; right: 20px"
+      @click="counter++"
+    >
+      {{ counter }}
+    </button>
     <input
       v-model="message"
       @keyup.esc="clearMessage"
       @keyup.enter="alertMessage"
+      v-autofocus
+      ref="inputField"
     />
 
     <button @click="clearMessage">Clear Text</button>
@@ -14,34 +23,74 @@
 
     <hr />
 
-    <p>Uppercase message: {{ uppercaseMessage() }}</p>
+    <p>Uppercase message: {{ uppercaseMessage }}</p>
+    <p>Lowercase message: {{ lowercaseMessage(message) }}</p>
   </q-page>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      message: 'I am VueJS!',
-    };
-  },
-  methods: {
-    clearMessage() {
-      this.message = '';
-    },
-    alertMessage() {
-      alert(this.message);
-    },
-    uppercaseMessage() {
-      return this.message.toUpperCase();
-    },
-  },
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const inputField = ref(null);
+
+const focusInput = () => {
+  console.log('here');
+  if (!inputField.value) {
+    inputField.value.focus();
+  }
 };
+
+onMounted(focusInput);
+
+// export default {
+//   data() {
+//     return {
+//       message: 'Learning Vue the hard way',
+//       counter: 0,
+//     };
+//   },
+//   computed: {
+//     uppercaseMessage() {
+//       console.log('messssss');
+//       return this.message.toUpperCase() + ' ' + this.counter;
+//     },
+//   },
+//   methods: {
+//     clearMessage() {
+//       this.message = '';
+//     },
+//     alertMessage() {
+//       alert(this.message);
+//     },
+//     lowercaseMessage(value) {
+//       return value.toLowerCase();
+//     },
+
+//   },
+//   directives: {
+//     autofocus: {
+//       inserted(el) {
+//         console.log('input inserted');
+//         el.focus();
+//       },
+//     },
+//   },
+// };
 </script>
 
 <style>
 .wrapper {
-  border: 5px double gray;
+  border: 5px double slategray;
   padding: 10px;
+}
+.btn {
+  padding-left: 20px;
+  padding-right: 20px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  background-color: slategray;
+  border: 1px solid black;
+  border-radius: 3px;
+  color: white;
 }
 </style>
